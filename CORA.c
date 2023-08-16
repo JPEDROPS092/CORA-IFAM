69d48601b65334342d458c65dfbec82db38b1eb4
alexandrecarloss <107065086+alexandrecarloss@users.noreply.github.com>
2023-08-16 15:36:55 -0400

    Adicionei novas structs e procedimento

--- CORA.c
+++ CORA.c
@@ -1,32 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+
 
 #define MAX_ARMARIOS 100
 #define MAX_ALUNOS 300
 
+
 typedef struct {
-    int numero;
+    int dia, mes, ano;
+} Data;
+
+typedef struct {
+    int numero; //Chave primária
     int status; // 0 - Disponível, 1 - Reservado, 2 - Em Uso
-    int aluno_associado;
+    char estado; // [s] = conservado, [n] = não conservado
+    int aluno_associado; //Essa associação é feita pela estrutura reserva
 } Armario;
 
 typedef struct {
-    int matricula;
-    char nome[100];
-    int armario_associado;
+    int matricula, turma; // Matrícula = Chave primária
+    char nome[100], email[20], fone[15];
+    int armario_associado; //Essa associação é feita pela estrutura reserva
 } Aluno;
 
+//Criação de vetores com o tamanho definido por constantes
 Armario armarios[MAX_ARMARIOS];
 Aluno alunos[MAX_ALUNOS];
+//Variáveis globais para controlar a quantidade de armários e alunos
 int num_armarios = 0;
 int num_alunos = 0;
 int next_aluno_id = 1;
 int next_armario_id = 1;
 
+typedef struct {
+    int matricula; // Matrícula = Chave primária
+    char nome[100], usuario[15], senha[10], email[20], fone[15];
+} Funcionario;
+
+typedef struct {
+    int codigo; // Código = Chave primária
+    char nome[100];
+} Curso;
+
+typedef struct {
+    int *matAluno, *codCurso, ano; // matAluno + codCurso = Chave primária
+    //matAluno = chave estrangeira para aluno, codCurso = chave estrangeira para curso
+    Data dataInicio, dataFim;
+} AlunoCurso;
+
+typedef struct {
+    int codigo, *matAluno, *matFunc, *numArmario; // codigo = Chave primária
+    //matAluno = chave estrangeira para aluno, matFunc = chave estrangeira para funcionário, numArmario = chave estrangeira para armario
+    Data dataRec, dataDev;
+} Reserva;
+
+//Procedimento para preencher informações do aluno
+void infAluno(Aluno *aluno) {
+    if (num_alunos < MAX_ALUNOS) {
+        aluno->matricula = next_aluno_id++;
+        printf("Informe o nome do aluno: ");
+        gets(aluno->nome);
+        printf("Informe o e-mail do aluno: ");
+        gets(aluno->email);
+        printf("Informe o telefone do aluno: ");
+        gets(aluno->fone);
+        printf("Informe a turma do aluno: ");
+        scanf("%d", &aluno->turma);
+        getchar();
+        next_aluno_id++;
+        printf("Aluno cadastrado e armário associado com sucesso!\n");
+    } else {
+        printf("Erro: Limite máximo de alunos atingido\n");
+    }
+}
+
 int encontrarArmarioDisponivel(int serie) {
     for (int i = 0; i < num_armarios; i++) {
-        if (armarios[i].status == 0 && armarios[i].aluno_associado == -1) {
+        if ((armarios[i].status == 0) && (armarios[i].aluno_associado == -1)) {
             armarios[i].status = 1;
             return i;
         }
