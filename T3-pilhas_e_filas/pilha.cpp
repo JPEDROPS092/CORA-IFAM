e7560ff6eb2cc5cb44f1878c05d8035bd9cbae7e
Carlos Alexandre <107065086+alexandrecarloss@users.noreply.github.com>
2023-11-12 11:28:50 -0400

    Criei funções de pilha
    Não tankei, fui fominha

--- T3-pilhas_e_filas/pilha.cpp
+++ T3-pilhas_e_filas/pilha.cpp
@@ -10,46 +10,144 @@ using namespace std;
 
 //ESTRUTURAS
 struct Numero {
-    float num;
-    struct Fruta *pAnt_num, *pProx_num;
-} Num1, Num2, Soma, *pAux, *pUlt_num1, *pUlt_num2, *pUlt_soma;
+    int num;
+    struct Numero *pProx_num;
+} numero;
 
-//*************************OPERAÇÕES*******************************
-//clear() limpa a lista
-void limpar(Numero pilha) {
-    pilha.pProx_num = NULL;
+struct Pilha {
+    Numero *topo;
+    int tamanho;
+} pilha;
+
+void criarPilhar(Pilha *p) {
+    p->topo = NULL;
+    p->tamanho = 0;
 }
 
+//********************************************OPERAÇÕES******************************************
+
+
 //isEmpty() verifica se a pilha esta vazia
-int estaVazio(Numero pilha) {
-    if(pilha.pProx_num == NULL){
-        return 1;
-    } else {
+int estaVazio(Pilha *p) {
+    if(p->tamanho > 0)
         return 0;
-    }
+    else
+        return 1;
 }
 
 //push(el) empilha no topo da lista
-void empilhar(Numero *ultimo, float elemento) {
-    ultimo->pProx_num = new Numero;
-    ultimo->pProx_num->pAnt_num = ultimo;
-    ultimo = ultimo->pProx_num;
-    ultimo->num = elemento;
-    ultimo->pProx_num = NULL;
+void empilhar(Pilha *p, int num) {
+    Numero *novo = new Numero;
+    if(novo){
+        novo->num = num;
+        novo->pProx_num = p->topo;
+        p->topo = novo;
+        p->tamanho++;
+    } else
+        cout << "\nErro ao alocar memória\n";
 }
 
 //pop() retira e retorna o mais alto
-Numero desempilhar (Numero *ultimo) {
+Numero* desempilhar(Pilha *p) {
+    if(p->topo) {
+        Numero *removido = p->topo;
+        p->topo = removido->pProx_num;
+        p->tamanho--;
+        return removido;
+    } else
+        cout << "\nPilha vazia!\n";
+    return NULL;
+}
 
+//clear() limpa a lista
+void limpar(Pilha *p) {
+    while(p->topo) {
+        desempilhar(p);
+    }
 }
 
 //topEl() retorna o mais alto sem removê-lo
-Numero ultimo(Numero *ultimo) {
+Numero* topo(Pilha *p) {
+    return p->topo;
+}
+
+//Receber número
+int receberNumero() {
+    int numero;
+    cout << "Número: ";
+    scanf("%d", &numero);
+    return numero;
+}
+
+//Listar pilha
+void listarPilha(Pilha *p) {
+    Numero *exibir = p->topo;
+    while(exibir) {
+        cout << exibir->num << " ";
+        exibir = exibir->pProx_num;
+    }
+}
+
+//Menu Principal
+void menu() {
+    int op = 1, novo;
+    Pilha pilha;
+    criarPilhar(&pilha);
+    do{
+       system("cls");
+       cout << "\t\tO QUE DESEJA FAZER?\n";
+       cout << "SAIR-0\nEMPILHAR-1\nDESEMPILHAR-2\nLIMPAR-3\nESTA VAZIA-4\nTOPO-5\nLISTAR PILHA-6\n:";
+       cin >> op;
+       switch(op){
+           case 0:
+               system("cls");
+               cout<<"PROGRAMA FINALIZADO";
+           break;
+           case 1:
+                system("cls");
+                novo = receberNumero();
+                empilhar(&pilha, novo);
+                system("pause");
+           break;
+           case 2:
+               system("cls");
+               desempilhar(&pilha);
+               system("pause");
+           break;
+           case 3:
+               system("cls");
+               limpar(&pilha);
+               system("pause");
+           break;
+           case 4:
+               system("cls");
+               if(estaVazio(&pilha))
+                    cout << "Pilha vazia";
+                else
+                    cout << "Pilha não está vazia";
+               system("pause");
+           break;
+           case 5:
+               system("cls");
+               cout << topo(&pilha)->num;
+               system("pause");
+           break;
+           case 6:
+               system("cls");
+               listarPilha(&pilha);
+               system("pause");
+           break;
+           default:
+            cout<<"\nOpção inválida, tente novamente!\n";
+            system("pause");
+           break;
+       }
+    }while(op!=0);
 
 }
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
-
+    menu();
     return 0;
 }
